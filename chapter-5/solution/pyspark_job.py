f2a3df083a94995cd369babb0ad5e1cadf13aad4
aarguello <andres.arguello@cherre.com>
2022-08-29 21:11:38 -0300

    chp5 pyspark_job

--- chapter-5/solution/pyspark_job.py
+++ chapter-5/solution/pyspark_job.py
@@ -1,8 +1,19 @@
 from pyspark.sql import SparkSession
 
+
+MASTER_NODE_INSTANCE_NAME="[packt-dataproc-cluster-m]"
+
 spark = SparkSession.builder \
     .appName('spark_hdfs_to_hdfs') \
     .getOrCreate()
 
 sc = spak.sparkContext
 sc.setLogLevel("WARN")
+
+log_files_rdd = sc.textFile(f'hdfs://{MASTER_NODE_INSTANCE_NAME}/data/logs_example/*')
+splitted_rdd = log_files_rdd.map(lambda x: x.split(" "))
+selected_col_rdd = splitted_rdd.map(lambda x: (x[0], x[3], x[5], x[6]))
+
+columns = ["ip","date","method","url"]
+logs_df = selected_col_rdd.toDF(columns)
+logs_df.createOrReplaceTempView('logs_df')
