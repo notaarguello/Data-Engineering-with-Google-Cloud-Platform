6c70590abb3860b4fbff95d87887cc27214fd5c3
aarguello <andres.arguello@cherre.com>
2022-08-29 21:30:51 -0300

    chp5 more pyspark job

--- chapter-5/solution/pyspark_job.py
+++ chapter-5/solution/pyspark_job.py
@@ -17,3 +17,17 @@ selected_col_rdd = splitted_rdd.map(lambda x: (x[0], x[3], x[5], x[6]))
 columns = ["ip","date","method","url"]
 logs_df = selected_col_rdd.toDF(columns)
 logs_df.createOrReplaceTempView('logs_df')
+
+sql = """
+  SELECT
+  url,
+  count(*) as count
+  FROM logs_df
+  WHERE url LIKE '%/article%'
+  GROUP BY url
+  """
+article_count_df = spark.sql(sql)
+print(" ### Get only articles and blogs records ### ")
+article_count_df.show(5)
+
+article_count_df.write.save(f'hdfs://{MASTER_NODE_INSTANCE_NAME}/data/logs_example/*', format='csv', mode='overwrite')
