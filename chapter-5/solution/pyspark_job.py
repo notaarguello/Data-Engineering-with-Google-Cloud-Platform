935958d1f24e92610f2292ab6186e056f9a282e7
aarguello <andres.arguello@cherre.com>
2022-08-30 16:49:45 -0300

    removed ip column

--- chapter-5/solution/pyspark_job.py
+++ chapter-5/solution/pyspark_job.py
@@ -12,9 +12,9 @@ sc.setLogLevel("WARN")
 
 log_files_rdd = sc.textFile(f'hdfs://{MASTER_NODE_INSTANCE_NAME}/data/logs_example/*')
 splitted_rdd = log_files_rdd.map(lambda x: x.split(" "))
-selected_col_rdd = splitted_rdd.map(lambda x: (x[0], x[3], x[5], x[6]))
+selected_col_rdd = splitted_rdd.map(lambda x: (x[3], x[5], x[6]))
 
-columns = ["ip_2","date","method","url"]
+columns = ["date","method","url"]
 logs_df = selected_col_rdd.toDF(columns)
 logs_df.createOrReplaceTempView('logs_df')
 
